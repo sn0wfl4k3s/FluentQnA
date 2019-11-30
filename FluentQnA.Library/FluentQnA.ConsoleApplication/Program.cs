0edea278ed88aa0175d6d927ccc4bbb95d23af8b
Flakes <ed.afram@gmail.com>
2019-11-30 18:39:57 -0300

    Inclusão de um exemplo para console application.

---
+++ FluentQnA.Library/FluentQnA.ConsoleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using FluentQnA;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                IFluentQnA service = new FluentQnAImpl();
+
+                service.LoadKnowledgebase();
+
+                service.TrainingModel();
+
+                var metrics = await service.GetAnswers("seu nome?", 0f);
+
+                foreach (var qna in metrics)
+                {
+                    Console.WriteLine($"Resposta: {qna.Answer} | Acuracia: {qna.Accuracy}");
+                }
+
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
